ecc04c1d0462b883cfd985c67f9def7a0253d97a
Michael Wieland <git@selfcoders.com>
2022-01-06 22:14:11 +0100

    Add config option open_links_in_default_browser for web_view widget

--- modules/web_view/__init__.py
+++ modules/web_view/__init__.py
@@ -1,14 +1,32 @@
+import webbrowser
+
 from PyQt5 import QtWebEngineWidgets, QtCore
 
 from lib.common import AbstractView
 
 
+class WebPage(QtWebEngineWidgets.QWebEnginePage):
+    def __init__(self, parent, open_links_in_default_browser: bool):
+        super().__init__(parent)
+
+        self.open_links_in_default_browser = open_links_in_default_browser
+
+    def acceptNavigationRequest(self, url: QtCore.QUrl, navigation_type: QtWebEngineWidgets.QWebEnginePage.NavigationType, is_main_frame: bool):
+        if navigation_type == QtWebEngineWidgets.QWebEnginePage.NavigationTypeLinkClicked and self.open_links_in_default_browser:
+            webbrowser.open(url.toString())
+            return False
+
+        return True
+
+
 class View(QtWebEngineWidgets.QWebEngineView, AbstractView):
-    def __init__(self, url, js=None, css=None):
+    def __init__(self, url, js=None, css=None, open_links_in_default_browser=False):
         super().__init__()
 
         self.js = js
         self.css = css
+
+        self.setPage(WebPage(self, open_links_in_default_browser))
 
         self.loadFinished.connect(self.loaded)
         self.load(QtCore.QUrl(url))
