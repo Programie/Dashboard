fb331b01e103773bf249fddb2fcd80a47a7e5a5f
Michael Wieland <git@selfcoders.com>
2022-07-10 22:38:53 +0200

    Automatically restart playback after suspend and on error (if enabled)

--- modules/media_player/__init__.py
+++ modules/media_player/__init__.py
@@ -6,7 +6,7 @@ from lib.common import AbstractView, get_dashboard_instance, is_visible
 
 
 class View(QtMultimediaWidgets.QVideoWidget, AbstractView):
-    def __init__(self, url, width=None, height=None, stop_on_inactive=True, auto_restart_playback=False):
+    def __init__(self, url, width=None, height=None, stop_on_inactive=True, auto_restart_playback=False, auto_restart_playback_on_error=False):
         super().__init__()
 
         self.url = url
@@ -17,6 +17,7 @@ class View(QtMultimediaWidgets.QVideoWidget, AbstractView):
         self.dbus_login_manager = QtDBus.QDBusInterface("org.freedesktop.login1", "/org/freedesktop/login1", "org.freedesktop.login1.Manager", self.dbus)
 
         self.dbus.connect("org.freedesktop.login1", "/org/freedesktop/login1/seat/seat0", "org.freedesktop.DBus.Properties", "PropertiesChanged", self.login_seat_changed)
+        self.dbus.connect("org.freedesktop.login1", "/org/freedesktop/login1", "org.freedesktop.login1.Manager", "PrepareForSleep", self.prepare_for_sleep_changed)
 
         if width and height:
             self.setFixedSize(QtCore.QSize(width, height))
@@ -27,11 +28,19 @@ class View(QtMultimediaWidgets.QVideoWidget, AbstractView):
         if auto_restart_playback:
             self.player.stateChanged.connect(self.state_changed)
 
+        if auto_restart_playback_on_error:
+            self.player.error.connect(self.on_error)
+
         if stop_on_inactive:
             self.visibility_changed.connect(self.update_state_by_visibility)
             get_dashboard_instance().window_state_changed.connect(self.update_state_by_visibility)
             get_dashboard_instance().screensaver_state_changed.connect(self.screensaver_state_changed)
         else:
+            self.play()
+
+    def on_error(self):
+        if self.playing:
+            self.stop()
             self.play()
 
     def state_changed(self, new_state: QtMultimedia.QMediaPlayer.State):
@@ -63,6 +72,13 @@ class View(QtMultimediaWidgets.QVideoWidget, AbstractView):
     def stop(self):
         self.playing = False
         self.player.stop()
+
+    @QtCore.pyqtSlot(bool)
+    def prepare_for_sleep_changed(self, state):
+        if not state and self.playing:
+            # Restart playback after waking up from suspend if previously played back
+            self.player.stop()
+            self.player.play()
 
     @QtCore.pyqtSlot("QString", "QVariantMap")
     def login_seat_changed(self, interface, properties):
